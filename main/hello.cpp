157087ff6c6ea1fc98c1933c62bf0ac9f804e3d1
samarth-clone <samarthbhaty@gmail.com>
2025-03-18 14:33:54 +0530

    First commit?????

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "What \n";
+  cout << "Hello, Jenkins \n";
+  cout << "I have successfully built and run this thing yipee :)  \n";
   return 0;
 }
