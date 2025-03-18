3608998ca33615ca65f7561d62c934e21a9d813c
samarth-clone <samarthbhaty@gmail.com>
2025-03-18 15:05:00 +0530

    pls build

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "What \n";
   cout << "Hello, Jenkins \n";
-  cout << "I have successfully built and run this thing yipee :)  \n";
+  cout << "I have successfully built and run this thing  WHAHAHAHAHAHAHA yipee :)  \n";
   return 0;
 }
